8f4e296a10f0ec6ac731acc2d4fcd098e34602ba
prawizard <venkateshvpprasad@gmail.com>
2020-03-29 22:36:19 +0530

    Adding code and template

---
+++ SLR.py
@@ -0,0 +1,40 @@
+#Simple Linear Regression
+#Importing the libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Importing the dataset
+
+dataset=pd.read_csv('C:\\Users\\DELL\\Desktop\\PRAWIZARD\\MachineLearning Udemy\\Machine Learning A-Z Template Folder\\Part 2 - Regression\\Section 4 - Simple Linear Regression\\Simple_Linear_Regression\\Salary_Data.csv')
+X=dataset.iloc[:, :-1].values
+y=dataset.iloc[:, 1].values
+
+#splitting the dataset into training set and test set
+from sklearn.cross_validation import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=1/3,random_state=0)
+
+#Fitting Simple Linear Regression to training set
+from sklearn.linear_model import LinearRegression
+regressor=LinearRegression()
+regressor.fit(X_train,y_train)
+
+#Predicting the test ser results
+y_pred=regressor.predict(X_test)
+
+#Visualizing the training set results
+plt.scatter(X_train,y_train,color='red')
+plt.plot(X_train,regressor.predict(X_train),color='blue')
+plt.title('Salary vs Experience(Training Set)')
+plt.xlabel('Years of Exp')
+plt.ylabel('Salary')
+plt.show()
+
+#Visualizing the test set results
+plt.scatter(X_test,y_test,color='red')
+plt.plot(X_train,regressor.predict(X_train),color='blue')
+plt.title('Salary vs Experience(Test Set)')
+plt.xlabel('Years of Exp')
+plt.ylabel('Salary')
+plt.show()
